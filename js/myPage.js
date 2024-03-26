49e71b607e7a2b8cc08ca8611a60aaefdcbab783
KIS940 <inseon0806@gmail.com>
2024-03-26 18:08:00 +0900

    프로필 수정시 버튼 및 카테고리 작업

--- js/myPage.js
+++ js/myPage.js
@@ -1,4 +1,8 @@
-const contents = ["modifyProfile","myWrited","saved","receiveLike"]
+const contents = ["modifyProfile","myWrited","saved","receiveLike"];
+const profileInfos = ["showNickname", "showPhoneNumber","showAddress","showEmail"];
+const modifyType = ["nickname", "phoneNumber","address","email"];
+const modifyBtn =["modifyNickname","modifyPhoneNumber","modifyAddress","modifyEmail"];
+const saveBtn =["nicknameBtn","phoneNumberBtn","addressBtn","emailBtn"];
 
 function SelectedContent(selectNum){
   for(let i = 0; i < contents.length; i++){
@@ -7,4 +11,30 @@ function SelectedContent(selectNum){
   }
   const selectedElement = document.getElementById(contents[selectNum]);
   selectedElement.classList.add("active");
+}
+
+function ModifyProfileBtn(btnType) {
+  for (let i = 0; i < profileInfos.length; i++) {
+    const spanElement = document.getElementById(profileInfos[i]);
+    const inputElement = document.getElementById(modifyType[i]);
+    const saveElement = document.getElementById(saveBtn[i]);
+    const modifyElement = document.getElementById(modifyBtn[i]);
+    if (btnType === i) {
+      if (spanElement.classList.contains("hidden")) {
+        spanElement.classList.remove("hidden");
+        inputElement.classList.add("hidden");
+        saveElement.classList.add("hidden");
+        modifyElement.classList.remove("hidden");
+      } else {
+        spanElement.classList.add("hidden");
+        inputElement.classList.remove("hidden");
+        saveElement.classList.remove("hidden");
+        modifyElement.classList.add("hidden");
+      }
+    }
+  }
+}
+
+function SaveBtn(btnType){
+
 }
