b949de081172b93a185e8f0b46c5fc4c6a170cc6
taein111 <taein4546@naver.com>
2024-03-28 20:38:59 +0900

    후기글상세페이지 및 경로 수정

--- js/event.js
+++ js/event.js
@@ -5,13 +5,13 @@
   const image_box3 = document.querySelector(".side_image3");
 
   image_box1.addEventListener("mouseover", function(){
-    change_image.src="img/1.png"
+    change_image.src="../img/1.png"
   });
   image_box2.addEventListener("mouseenter", function(){
-    change_image.src="img/2.png"
+    change_image.src="../img/2.png"
   });
   image_box3.addEventListener("mouseenter", function(){
-    change_image.src="img/3.png"
+    change_image.src="../img/3.png"
   });
 
   //  image_box1.addEventListener("mouseleave", function(){
