0dd28b18110355eea47d1e1665508591daea8e03
TERRY991223 <chfhrl99@naver.com>
2024-04-03 15:22:32 +0900

    메뉴완료

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,13 @@
-document.getElementById('menutap').addEventListener('click', function() {
-  var menuTap = document.getElementById('menutap');
-  if (menuTap.style.display === 'none') {
-      menuTap.style.display = 'block'; // 메뉴를 보이게 함
-      this.textContent = '메뉴 닫기';
+function activeTab(){
+  const drop = document.getElementById('menuTapPage');
+  const headerColor = document.getElementById('mainPColor');
+  if(drop.style.display === 'none') {
+      drop.style.display = 'block';
+      headerColor.style.backgroundColor = '#063F3B';
   } else {
-      menuTap.style.display = 'none'; // 메뉴를 숨김
-      this.textContent = '메뉴 열기';
+    drop.style.display = 'none';
+    headerColor.style.backgroundColor = 'transparent';
+  
   }
-});
+  
+}
