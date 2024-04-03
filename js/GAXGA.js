73c8e6816e2d6d8f82b475b036a4d6b566f2aca6
Jangsen <wkdtjdgks77@naver.com>
2024-04-03 16:49:28 +0900

    폰트 이벤트 수정

--- js/GAXGA.js
+++ js/GAXGA.js
@@ -3,128 +3,128 @@ const imageElement = document.getElementById("image");
 const textElement1 = document.getElementById("text0");
 
 textElement1.addEventListener("mouseover", function() {
-  imageElement.src= "img/seoulcafe1.png";
+  imageElement.src= "../img/seoulcafe1.png";
 });
 
 const textElement2 = document.getElementById("text2");
 
 textElement2.addEventListener("mouseover", function() {
-  imageElement.src = "img/seoulcafe2.png";
+  imageElement.src = "../img/seoulcafe2.png";
 });
 const textElement3 = document.getElementById("text3");
 
 textElement3.addEventListener("mouseover", function() {
-  imageElement.src = "img/seoulcafe3.png";
+  imageElement.src = "../img/seoulcafe3.png";
 });
 const textElement4 = document.getElementById("text4");
 
 textElement4.addEventListener("mouseover", function() {
-  imageElement.src = "img/namsan.png";
+  imageElement.src = "../img/namsan.png";
 });
 const textElement5 = document.getElementById("text5");
 
 textElement5.addEventListener("mouseover", function() {
-  imageElement.src = "img/wolmido.png";
+  imageElement.src = "../img/wolmido.png";
 });
 const textElement6 = document.getElementById("text6");
 
 textElement6.addEventListener("mouseover", function() {
-  imageElement.src = "img/Oworld.png";
+  imageElement.src = "../img/Oworld.png";
 });
 const textElement7 = document.getElementById("text7");
 
 textElement7.addEventListener("mouseover", function() {
-  imageElement.src = "img/kingsejong.png";
+  imageElement.src = "../img/kingsejong.png";
 });
 const textElement8 = document.getElementById("text8");
 
 textElement8.addEventListener("mouseover", function() {
-  imageElement.src = "img/botanicgarden.png";
+  imageElement.src = "../img/botanicgarden.png";
 });
 const textElement9 = document.getElementById("text9");
 
 textElement9.addEventListener("mouseover", function() {
-  imageElement.src = "img/childgrandpark.png";
+  imageElement.src = "../img/childgrandpark.png";
 });
 const textElement10 = document.getElementById("text10");
 
 textElement10.addEventListener("mouseover", function() {
-  imageElement.src = "img/seoulcafe1.png";
+  imageElement.src = "../img/seoulcafe1.png";
 });
 const textElement11 = document.getElementById("text11");
 
 textElement11.addEventListener("mouseover", function() {
-  imageElement.src = "img/seoulcafe2.png";
+  imageElement.src = "../img/seoulcafe2.png";
 });
 const textElement12 = document.getElementById("text12");
 
 textElement12.addEventListener("mouseover", function() {
-  imageElement.src = "img/seoulcafe3.png";
+  imageElement.src = "../img/seoulcafe3.png";
 });
 const textElement13 = document.getElementById("text13");
 
 textElement13.addEventListener("mouseover", function() {
-  imageElement.src = "img/pajeonstreet.png";
+  imageElement.src = "../img/pajeonstreet.png";
 });
 const textElement14 = document.getElementById("text14");
 
 textElement14.addEventListener("mouseover", function() {
-  imageElement.src = "img/jokbalalley.png";
+  imageElement.src = "../img/jokbalalley.png";
 });
 const textElement15 = document.getElementById("text15");
 
 textElement15.addEventListener("mouseover", function() {
-  imageElement.src = "img/tteokbokkitown.png";
+  imageElement.src = "../img/tteokbokkitown.png";
 });
 const textElement16 = document.getElementById("text16");
 
 textElement16.addEventListener("mouseover", function() {
-  imageElement.src = "img/gamaksan.png";
+  imageElement.src = "../img/gamaksan.png";
 });
 const textElement17 = document.getElementById("text17");
 
 textElement17.addEventListener("mouseover", function() {
-  imageElement.src = "img/gapyeonghyang-gyo.png";
+  imageElement.src = "../img/gapyeonghyang-gyo.png";
 });
 const textElement18 = document.getElementById("text18");
 
 textElement18.addEventListener("mouseover", function() {
-  imageElement.src = "img/hangugminsogchon.png";
+  imageElement.src = "../img/hangugminsogchon.png";
 });
 const textElement19 = document.getElementById("text19");
 
 textElement19.addEventListener("mouseover", function() {
-  imageElement.src = "img/cafesoom.png";
+  imageElement.src = "../img/cafesoom.png";
 });
 const textElement20 = document.getElementById("text20");
 
 textElement20.addEventListener("mouseover", function() {
-  imageElement.src = "img/jeilsijang.png";
+  imageElement.src = "../img/jeilsijang.png";
 });
 const textElement21 = document.getElementById("text21");
 
 textElement21.addEventListener("mouseover", function() {
-  imageElement.src = "img/cheonglyangsan2.png";
+  imageElement.src = "../img/cheonglyangsan2.png";
 });
 const textElement22 = document.getElementById("text22");
 
 textElement22.addEventListener("mouseover", function() {
-  imageElement.src = "img/cheonglyangsan1.png";
+  imageElement.src = "../img/cheonglyangsan1.png";
 });
 const textElement23 = document.getElementById("text23");
 
 textElement23.addEventListener("mouseover", function() {
-  imageElement.src = "img/incheonsanglyugjagjeon.png";
+  imageElement.src = "../img/incheonsanglyugjagjeon.png";
 });
 const textElement24 = document.getElementById("text24");
 
 textElement24.addEventListener("mouseover", function() {
-  imageElement.src = "img/wolmido.png";
+  imageElement.src = "../img/wolmido.png";
 });
 const textElement25 = document.getElementById("text25");
 
 textElement25.addEventListener("mouseover", function() {
-  imageElement.src = "img/forestoutings.png";
+  imageElement.src = "../img/forestoutings.png";
 });
 
 // mouseover
