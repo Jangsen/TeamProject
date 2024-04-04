d4ac1403b897244c6f0d0a77994b33c399ac9ece
KIS940 <inseon0806@gmail.com>
2024-04-04 18:35:40 +0900

    버튼 맨처음만 클릭실행안되는 문제 해결

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 function activeTab(){
   const drop = document.getElementById('menuTapPage');
   const headerColor = document.getElementById('mainPColor');
-  if(drop.style.display === 'none') {
+  if(drop.style.display === 'none' || drop.style.display === '') {
       drop.style.display = 'block';
       headerColor.style.backgroundColor = '#063F3B';
   } else {
