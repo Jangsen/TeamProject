2d5baf1aa3f7f956fe1ccc8b41633ce24bc408a4
taein111 <taein4546@naver.com>
2024-04-08 18:21:50 +0900

    20240408

---
+++ BE/gaxga/src/main/java/com/teamproject/gaxga/controller/GabowatdagoController.java
@@ -0,0 +1,9 @@
+package com.teamproject.gaxga.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class GabowatdagoController {
+
+}
