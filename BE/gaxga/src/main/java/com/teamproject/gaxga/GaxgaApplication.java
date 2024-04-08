a923a11045bb136a4433b80524400fb9797450d9
KIS940 <inseon0806@gmail.com>
2024-04-08 14:47:26 +0900

    스프링부트 분류

---
+++ BE/gaxga/src/main/java/com/teamproject/gaxga/GaxgaApplication.java
@@ -0,0 +1,13 @@
+package com.teamproject.gaxga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaxgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaxgaApplication.class, args);
+	}
+
+}
