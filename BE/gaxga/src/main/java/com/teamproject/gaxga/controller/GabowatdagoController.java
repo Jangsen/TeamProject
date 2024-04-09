a6d39965289b3e57d99e961e1ad546dc2251ec9d
taein111 <taein4546@naver.com>
2024-04-09 14:41:32 +0900

    프로퍼티즈 설정

--- BE/gaxga/src/main/java/com/teamproject/gaxga/controller/GabowatdagoController.java
+++ BE/gaxga/src/main/java/com/teamproject/gaxga/controller/GabowatdagoController.java
@@ -1,9 +1,13 @@
 package com.teamproject.gaxga.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class GabowatdagoController {
-
+    @GetMapping("/gabowatdago/gabowatdagoing_p")
+    public String newForm(){
+        return "gabowatdago/gabowatdagoing_p";
+    }
 }
