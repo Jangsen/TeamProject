0fcc984f62fc6077d9c94c194faf277cf0228425
taein111 <taein4546@naver.com>
2024-04-11 17:44:42 +0900

    20240411 base엔티티 가봤다고 작성페이지 수정중

--- BE/gaxga/src/main/java/com/teamproject/gaxga/dto/GabowatdagoForm.java
+++ BE/gaxga/src/main/java/com/teamproject/gaxga/dto/GabowatdagoForm.java
@@ -1,6 +1,7 @@
 package com.teamproject.gaxga.dto;
 
 
+//import com.teamproject.gaxga.entity.Gabowatdago;
 import com.teamproject.gaxga.entity.Gabowatdago;
 import lombok.AllArgsConstructor;
 import lombok.ToString;
@@ -9,19 +10,17 @@ import org.springframework.web.multipart.MultipartFile;
 @AllArgsConstructor
 @ToString
 public class GabowatdagoForm {
-    private Long id;
+
     private String title;
     private String local;
-    private String tema;
+    private String thema;
     private String image;
-    private String date;
-    private String writer;
     private String content;
     private String address;
     private String de_address;
 
 
     public Gabowatdago toEntity() {
-        return new Gabowatdago(id,title,local,tema,image,date,writer,content,address,de_address);
+        return new Gabowatdago(null, title, local, thema, image, content, address, de_address);
     }
 }
